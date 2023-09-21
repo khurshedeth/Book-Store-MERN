62356cb30d52b3bb67e74d562ac10d631a2da347
khurshedeth <khurshed.eth@gmail.com>
2023-09-21 23:45:50 +0530

    chanes update

---
+++ components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+    
+    <h1>Book store</h1>
+    <input placeholder='Enter Book Name'/>
+    </div>
+  )
+}
+
+export default Form
